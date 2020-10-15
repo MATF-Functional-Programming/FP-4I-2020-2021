765ee85d358eb1c9fa474ab30604ae8cb2a559a5
ivan-ristovic <ivan.ristovic95@gmail.com>
2020-10-15 22:21:54 +0200

    Week 1

---
+++ 01-hs-ring/01_intro/src/Main.hs
@@ -0,0 +1,149 @@
+import Data.Char
+
+-----------------------------------------------------------------------------
+-- Fold (reduce, accumulate) ------------------------------------------------
+-----------------------------------------------------------------------------
+
+-- Obicna rekurzija
+mySum :: Num a => [a] -> a
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+-- Repna rekurzija -- u svakom pozivu prosledjujemo
+-- prethodnu akumuliranu vrednost
+mySum' :: Num a => [a] -> a
+mySum' xs =
+    let mySumHelper acc values =
+                    case values of
+                         [] -> acc
+                         (x:xs) -> mySumHelper (acc + x) xs
+    in mySumHelper 0 xs
+
+-- Fold je genericki algoritam koji implementira
+-- repnu rekurziju
+mySum'' xs =
+    foldl (+) 0 xs
+
+-- Filtriranje rekurzijom
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter p (x:xs) = let filteredTail = myFilter p xs
+                    in if p x then x: filteredTail
+                              else filteredTail
+
+-- Filtriranje repnom rekurzijom
+myFilter' :: (a -> Bool) -> [a] -> [a]
+myFilter' _ [] = []
+myFilter' p xs =
+    let myFilterHelper acc values =
+                       case values of
+                            [] -> acc
+                            (x:xs) -> myFilterHelper (if p x then x:acc else acc) xs
+    in myFilterHelper [] (reverse xs)
+
+-- Filtriranje kroz fold
+myFilter'' :: (a -> Bool) -> [a] -> [a]
+myFilter'' p xs =
+    foldr (\ x acc -> if p x then x:acc else acc) [] xs
+
+
+-- Implementacija konkatenacije preko folda
+myConcat :: [a] -> [a] -> [a]
+myConcat ls rs =
+    foldr (:) rs ls
+
+
+
+
+--
+-- Ekstraktujemo jednu rec iz recenice
+--
+
+-- ne ignorise praznine na početku stringa
+takeWord' :: String -> String
+takeWord' sentence = takeWhile (not . isSpace) sentence
+
+-- ignorišemo praznine, ali nije mnogo čitljivo rešenje
+takeWord'' :: String -> String
+takeWord'' sentence = takeWhile (not . isSpace) (dropWhile isSpace sentence)
+
+-- pointfree verzija funkcije
+takeWord :: String -> String
+takeWord = takeWhile (not . isSpace) . dropWhile isSpace
+
+--
+takeWordVerbose :: String -> String
+takeWordVerbose =
+        let isNotSpace = not . isSpace
+        in  takeWhile isNotSpace . dropWhile isSpace
+
+
+
+--
+-- Ekstraktujemo prve dve reci iz recenice
+--
+
+-- ako želimo da ekstraktujemo prve dve reči, moramo da
+-- zapamtimo ostatak stringa posle izvlačenja prve reči
+breakWord :: String -> (String, String)
+breakWord = break isSpace . dropWhile isSpace
+
+-- možemo da spajamo pozive prethodne funkcije koliko
+-- god puta želimo (@see Monade)
+breakWord2 :: String -> (String, String, String)
+breakWord2 sentence =
+        let (word1, rest1) = breakWord sentence
+            (word2, rest2) = breakWord rest1
+        in  (word1, word2, rest2)
+
+-- postoje ugradjene f-je koje razbijaju stringove
+-- na linije i reči, kao i njihove inverzne f-je:
+-- words, lines, unwords, unlines
+
+
+
+--
+-- Imamo listu, hoćemo da je filtriramo u odnosu
+-- na indekse elemenata
+--
+
+filterByIndex p xs =
+    let withIndices xs = zip [1..] xs    -- zipujemo xs sa beskonačnom
+        indexed        = withIndices xs  -- listom celih brojeva
+        pairpred       = p . fst         -- primenjujemo predikat na prvu komponentu redjenog para
+    in map snd $ filter pairpred indexed
+
+
+--
+-- Želimo da napravimu f-ju koja računa vrednosti liste
+-- f-ja u odredjenoj tački
+-- applyFunctions (pi/3) [sin, cos, tan, atan]
+--
+
+applyFunctions'  x fns = map (\ f -> f x) fns
+applyFunctions   x fns = map ($x) fns
+applyFunctionsPF       = map . flip id
+
+
+
+-- Cuvari (guards) su lepsi zapis za lanac if else if else if naredbi
+accumulate :: (Eq a, Num a) => (a -> a -> a) -> [a] -> a
+accumulate f xs
+        | xs == []   = 0
+        | otherwise  = f (head xs) (accumulate f $ tail xs)
+
+-- Mogu da sadrze i pattern matching izraze
+-- (linije koje sadrze <- xs)
+accumulate' :: (Eq a, Num a) => (a -> a -> a) -> [a] -> a
+accumulate' f xs
+        | xs == []       = 0
+        | (x:[])  <- xs  = x
+        | (x:xs)  <- xs  = f x (accumulate' f xs)
+
+
+
+main :: IO ()
+main = putStrLn "Hello"
+
+
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# MATF-Functional-Programming
+
+Materials for Functional Programming course
+
+> Our Haskell, who art in binary,  
+> hallowed be thy type system,  
+> thy monoids come,  
+> thy will be done,  
+> on VSCode as is in emacs.  
+> Give us this day our daily recursion  
+> and forgive us our side effects and infinite loops,  
+> and lead us not into imperative languages,  
+> but deliver us from mutation,  
+> for thine is the type system and the monoids  
+> and the pure functions, forever.  
+> 
+> In the name of map, fold and monadic bind,  
+> Amen >>=

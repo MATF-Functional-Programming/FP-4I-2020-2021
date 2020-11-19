f2c8ab6b4ee1d762f57702bfde13e2786807f785
ivan-ristovic <ivan.ristovic95@gmail.com>
2020-11-19 12:15:36 +0100

    Week 6

---
+++ 06-hs-word-count/01_word-count/src/Main.hs
@@ -0,0 +1,42 @@
+import System.Environment
+
+import qualified Data.List as List
+import qualified Data.Char as Char
+
+import Data.Tuple
+
+-- IO a - omotacki tip koji predstavlja runtime akciju
+--        koja vraca/generise instancu tipa a
+
+main :: IO ()
+main = do
+    -- Učitavamo stdin u listu
+    text <- getContents -- IO String
+
+    -- Učitavamo prvi argument komandne linije, ostale ignorišemo
+    -- (ne proveravamo da li argument postoji)
+    (arg: _) <- getArgs
+ 
+    -- Konvertujemo argument u broj
+    let n = read arg :: Int
+
+    -- Izlazimo iz 'nečistog' dela programa
+    putStrLn $ process n text
+
+
+process :: Int -> String -> String
+process n text =
+    let -- Uklanjamo neželjene karaktere, i delimo ulaz na reči
+        ws = words $ map Char.toLower $
+                     map (\ c -> if Char.isLetter c then c else ' ') $ text
+
+        -- Računamo broj pojavljivanja svake reči
+        word_occs = map (\group -> (group !! 0, List.length group)) $ List.group $ List.sort ws
+
+        -- Sortiramo listu po broju pojavljivanja reči
+        sorted_by_occs = List.sortBy (flip compare) $ map swap word_occs
+
+    -- Ispisujemo prvih n rezultata
+    in unlines $ map show $ take n $ sorted_by_occs
+
+
